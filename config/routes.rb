08066702dc17e9ee9039c74dc4f47c53725a6b1c
L F <franco100lucas@gmail.com>
2024-07-04 21:16:51 -0300

    refactor: Update routes.rb to use resources for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
 
 Rails.application.routes.draw do
   root "articles#index" # Definindo como rota raiz a página de listagem de artigos
 
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show"
 
-  # Para mais detalhes da DSL disponível para esse arquivo, veja https://guides.rubyonrails.org/routing.html
+  resources :articles # Cria todas as rotas necessárias para um recurso RESTful
+
 end

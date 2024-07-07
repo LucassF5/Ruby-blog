a888ea782873a9ed9329e612cfcccd5bcf75fb0b
L F <franco100lucas@gmail.com>
2024-07-06 23:43:20 -0300

    feat: feat: Implementing DELETE on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,15 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment =@article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+      redirect_to article_path(@article), status: :see_other
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible # Adiciona módulo Visible
 
-  has_many :comments # Adiciona relacionamento de 1 para muitos com a tabela comments
+  has_many :comments, dependen: :destroy # Adiciona relacionamento com Comment e define que ao deletar um Article, os Comments relacionados também serão deletados
 
   validates :title, presence: true # Adiciona validação de presença
   validates :body, presence: true, length: { minimum: 10} # Adiciona validação de tamanho mínimo de 10 caracteres
